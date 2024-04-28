79d5cae105e4d2691752f6228278eca7e80228e0
Barath0729 <kumarbarath960@gmail.com>
2024-04-28 11:34:31 +0530

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import AboutImg from '../assets/about.png';
+
+export default function About () {
+    const config  = {
+        line1: 'Hi, My name is Barath. I am a Devops Engineer. I built PipeLines in Maven and Git Actions .',
+        line2: 'I am proficient in AWS Features Like EC-2, Lambda, s-3 Buckets,IAM and Many More .',
+        line3: 'In FrontEnd I know React js and In BackEnd, I know node js, Express.js, and MongoDB .'
+    }
+
+    return <section className='flex flex-col md:flex-row bg-secondary px-5' id='about'>
+        <div className='py-5 md:w-1/2'>
+            <img src={AboutImg} />
+        </div>
+        <div className='md:w-1/2 flex justify-center'>
+            <div className='flex flex-col justify-center text-white'>
+                <h1 className='text-4xl border-b-4 border-primary mb-5 w-[170px] font-bold'>About Me</h1>
+                <p className='pb-5'>{config.line1}</p>
+                <p className='pb-5'>{config.line2}</p>
+                <p className='pb-5'>{config.line3}</p>
+            </div>
+        </div>
+    </section>
+}
---
+++ src/components/Hero.js
@@ -0,0 +1,29 @@
+import HeroImg from '../assets/hero.png';
+import { AiOutlineTwitter, AiOutlineLinkedin } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
+
+export default function Hero() {
+    const config  = {
+        subtitle: 'Im a Full-stack developer and Designer',
+        social: {
+            twitter: 'https://twitter.com/Barath292907',
+            github: 'https://github.com/Barath0729',
+            linkedin: 'https://in.linkedin.com/in/barath2929'
+        }
+    }
+
+
+    return <section className='flex flex-col md:flex-row px-5 py-32 bg-primary justify-center'>
+       <div className='md:w-1/2 flex flex-col'>
+            <h1 className=' text-white text-6xl font-hero-font'>Hi, <br/> Im Barath 
+                
+            </h1>
+            <div className='flex py-10'>
+                <a href={config.social.twitter} className='pr-5 hover:text-white'><AiOutlineTwitter size={40} /></a>
+                <a href={config.social.github} className='pr-5 hover:text-white'><FaGithub size={40} /></a>
+                <a href={config.social.linkedin} className='hover:text-white'><AiOutlineLinkedin size={40}/></a>
+            </div>
+       </div>
+       <img className='md:w-1/3' src={HeroImg} />
+    </section>
+}
